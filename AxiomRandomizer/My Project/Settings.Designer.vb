6ebf47b36bd62a3f6d8ebf81bafca681559323a5
Christopher D Couture <videogameroulette@yahoo.ca>
2020-03-25 01:42:22 -0400

    Update 0.7.5.2

--- AxiomRandomizer/My Project/Settings.Designer.vb
+++ AxiomRandomizer/My Project/Settings.Designer.vb
@@ -176,10 +176,10 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
-        Public Property SteamVersion() As Boolean
+         Global.System.Configuration.DefaultSettingValueAttribute("0")>  _
+        Public Property SteamVersion() As Integer
             Get
-                Return CType(Me("SteamVersion"),Boolean)
+                Return CType(Me("SteamVersion"),Integer)
             End Get
             Set
                 Me("SteamVersion") = value
